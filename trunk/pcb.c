820d4bebe401a54bb60dbea1ccc4338f4005a782
anelson7@uw.edu <anelson7@uw.edu@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-05-26 22:06:41 +0000

    added test main that is commented out in PCB.c & PCB is working as it should.

--- trunk/pcb.c
+++ trunk/pcb.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include "pcb.h"
 #include "process.h"
 #include "pcb.h"
 
@@ -20,7 +19,6 @@ PCBPtr PCBConstructor(int id, int type, int IOCount) {
 	apcb->pid = id;
 	apcb->count = rand() % 20;
 	apcb->curr_count = apcb->count;
-	//apcb->srvc_calls = createServiceCalls();
 	apcb->state = 1;
 	apcb->process = ProcessConstructor(type, IOCount);
 	apcb->waiting_on = -1;
@@ -28,38 +26,34 @@ PCBPtr PCBConstructor(int id, int type, int IOCount) {
 
 	return apcb;
 }
-/*
-int createServiceCalls() {
-	srand(time(NULL));
-	int list[10];
-	int j;
-	for (j = 0; j < 10; j++) {
-		list[j] = rand() % 30;
-	}
-	return list;
-}
-*/
+
 void PCBDestructor(PCBPtr pcb) {
 	free(pcb);
 }
-/*
-//decriments the current count, if the current count is 0, it resets to the process count.
-void decrimentCount(PCBPtr PCB) {
-	PCBStr thePCB = *PCB;
-	int count = thePCB->curr_count;
-	if (count > 0) {
-		thePCB->curr_count = count - 1;
-	} else if (count == 0) {
-		thePCB->curr_count = thePCB->count;
-	}
-}
 
-int getCurrentCount(PCBPtr PCB) {
-	PCBStr thePCB = *PCB;
-	return thePCB->curr_count;
-}
-*/
 int getPCBId(PCBPtr pcb) {
-	//PCBStr thePCB = *pcb;
 	return pcb->pid;
 }
+/*
+void main(argc, argv) {
+	PCBPtr aptr = PCBConstructor(1, 1, 2);
+	int pcbtype = aptr->process->proc_type;
+	printf("\nproc id: ");
+	if (pcbtype == 0) {
+		printf("compute");
+	} else if (pcbtype == 1) {
+		printf("io");
+	}else if (pcbtype == 2) {
+		printf("keyboard");
+	}else if (pcbtype == 3) {
+		printf("producer");
+	}else if (pcbtype==4) {
+		printf("consumer");
+	}
+	printf("%d", aptr->pid);
+	printf("\ncount number: %d", aptr->count);
+	printf("\ncurrent count: %d", aptr->curr_count);
+	printf("\nstate: %d", aptr->state);
+	printf("\nwaiting on: %d", aptr->waiting_on);
+	printf("\nowns: %d", aptr->owns);
+}*/
