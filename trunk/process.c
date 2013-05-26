cc67ee3774fe16cda362c69b400ebd348a48727a
anelson7@uw.edu <anelson7@uw.edu@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-05-26 20:22:41 +0000

    fixed process files so that they work properly.

--- trunk/process.c
+++ trunk/process.c
@@ -1,20 +1,16 @@
 /*
  * process.c
-
  *
- *  Created on: May 23, 2013
+ *  Created on: May 25, 2013
  *      Author: Toshiba Home
  */
 
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-//#include "pcb.h"
 #include "process.h"
-//#include "pcb.h"
 
-ProcessPtr ProcessConstructor(int type, int numreq) {
-
+ProcessPtr ProcessConstructor(int type, int numofio) {
 
 	ProcessPtr p = (ProcessPtr) malloc(sizeof(ProcessStr));
 
@@ -23,16 +19,24 @@ ProcessPtr ProcessConstructor(int type, int numreq) {
 	srand(time(NULL));
 
 	p->no_steps = (rand() % 50) + 1;
-	p->no_requests = numreq;
-	int requests[numreq];
-	int *reqPtr = requests;
-	if (type == 1 && numreq > 0) {
-		int i;
-		for(i = 0; i < numreq; i++) {
-			requests[i] = (rand() % 50) + 1;
+	p->no_requests = numofio;
+	int requestslist[50];
+	int i;
+	for (i = 0; i < 50; i++) {
+		if (type == 1 && numofio > 0) {
+			int j;
+			for(j = 0; j < numofio; j++) {
+				requestslist[i] = (rand() % 50) + 1;
+			}
+		} else {
+			int k;
+			for(k = 0;k < numofio; k++) {
+				requestslist[i] = -1;
+			}
 		}
 	}
-	p->requests = reqPtr;
+	p->requests = requestslist;
+
 	return p;
 }
 
