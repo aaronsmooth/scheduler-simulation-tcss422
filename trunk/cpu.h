cbab35b6baee8e7010f1b0e457b04d425073b436
carr.michaelt@gmail.com <carr.michaelt@gmail.com@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-06-01 23:39:06 +0000

    Updated comments, removed int IOReq (dont believe this was being used anymore)

--- trunk/cpu.h
+++ trunk/cpu.h
@@ -2,37 +2,37 @@
  *	cpu.h
  *	
  *	Created on: 5/29/2013
- *	Author:	Michael Carr
+ *	Author:	Aaron Nelson, Ankita Tiku, Michael Carr
  */
 
 #ifndef CPU_H_
 #define CPU_H_
 
 typedef struct {
-	int timerINT; //0 = no interrupt, 1 = interrupt;
-	int IO1Comp;
-	int IO2Comp;
-	int IOReq;
-	int KbComp;
-	int KbReq;
+	int timerINT; //0 = no interrupt, 1 = interrupt
+	int IO1Comp; //0 = no interrupt, 1 = interrupt
+	int IO2Comp; //0 = no interrupt, 1 = interrupt
+	//int IOReq; //needed?
+	int KbComp; //0 = no interrupt, 1 = interrupt
+	int KbReq; //0 = no request trap, 1 = request trap
 	int schedulerReady;	//0 = ready, 1 = not ready
 	QueuePtr schedulerQueue;
-	int count;
-	int runCount;
-	int isRunning;		//interrupt status
-	PCBPtr runningPCB;
-	QueuePtr ReadyQueue, KeyboardQueue, IoQueue;
-	mutexPtr Mutex;
-	int memoryLocations[];
-	KBDevPtr Keyboard;
-	IODevPtr IO1;
-	IODevPtr IO2;
+	int count; //total time count, between 1000 and 10000
+	int runCount; //current run count
+	int isRunning; //interrupt status, 1 = running, 0 = not running
+	PCBPtr runningPCB; //the currently running PCB in the CPU
+	QueuePtr ReadyQueue, KeyboardQueue, IoQueue; //Queues for respective structures
+	mutexPtr Mutex; //Array of Mutex locks
+	int memoryLocations[]; //Memory location for P|C data
+	KBDevPtr Keyboard; //Keyboard structure
+	IODevPtr IO1; //IO Device 1 structure
+	IODevPtr IO2; //IO Device 2 structure
 
-	pthread_t cpu_thread;
-	pthread_cond_t timer;
-	pthread_cond_t keyboarddone;
-	pthread_cond_t switchcomplete;
-	pthread_mutex_t mutex;
+	pthread_t cpu_thread; //cpu thread
+	pthread_cond_t timer; //???????
+	pthread_cond_t keyboarddone; //???????
+	pthread_cond_t switchcomplete; //???????
+	pthread_mutex_t mutex; //???????
 
 } cpuObj;
 
