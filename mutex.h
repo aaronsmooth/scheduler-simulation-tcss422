37c8ee6d0544f178c00501418f9ed289610b6f7b
anelson7@uw.edu <anelson7@uw.edu@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-06-03 06:40:17 +0000

    MyVersion2

---
+++ mutex.h
@@ -0,0 +1,38 @@
+/*
+ * mutex.h
+ *  This is the header file for a mutex object.
+ *  Created on: May 26, 2013
+ *      Author: Aaron Nelson
+ */
+
+#include "process.h"
+
+#ifndef MUTEX_H_
+#define MUTEX_H_
+
+typedef struct {
+		int mutexID;
+		PCBPtr owner;
+		int mutexLocked; //0 = unlocked, 1 = locked
+		PCBPtr waitingPCB;
+} mutexObj;
+
+typedef mutexObj *mutexPtr;
+
+mutexPtr mutexConstructor(int);
+
+void mutexDestructor(mutexPtr);
+
+void setOwner(mutexPtr, PCBPtr);
+
+void switchOwner(mutexPtr);
+
+void mutexEnqueue(mutexPtr, PCBPtr);
+
+int checkLock(mutexPtr);
+
+void mutexSwitch(mutexPtr, int);
+
+void printMutex(mutexPtr);
+
+#endif /* MUTEX_H_ */
