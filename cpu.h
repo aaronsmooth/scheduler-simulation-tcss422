285841df9429501f1d7d2ecf79046466d1196d97
anelson7@uw.edu <anelson7@uw.edu@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-06-03 06:37:20 +0000

    Version2

---
+++ cpu.h
@@ -0,0 +1,59 @@
+/*
+ *	cpu.h
+ *	
+ *	Created on: 5/29/2013
+ *	Author:	Aaron Nelson, Ankita Tiku, Michael Carr
+ */
+
+#ifndef CPU_H_
+#define CPU_H_
+
+ #include "process.h"
+ #include "global.h"
+ #include "mutex.h"
+ #include "Keyboard.h"
+
+typedef struct {
+
+	int count; //total time count, between 1000 and 10000
+	int runCount; //current run count
+	int isRunning; //interrupt status, 1 = running, 0 = not running
+	//int timerINT; //0 = no interrupt, 1 = interrupt
+	//int IO1Comp; //0 = no interrupt, 1 = interrupt
+	//int IO2Comp; //0 = no interrupt, 1 = interrupt
+	//int KbComp; //0 = no interrupt, 1 = interrupt
+	//int KbReq; //0 = no request trap, 1 = request trap
+	int schedulerReady;	//0 = ready, 1 = not ready
+	int memoryLocations; //Memory location for P|C data
+	PCBPtr runningPCB; //the currently running PCB in the CPU
+	QueuePtr schedulerQueue;
+	QueuePtr ReadyQueue, KeyboardQueue, IoQueue; //Queues for respective structures
+
+
+	mutexPtr Mutex; //Array of Mutex locks
+	KBDevPtr Keyboard; //Keyboard structure
+	IODevPtr IO1; //IO Device 1 structure
+	IODevPtr IO2; //IO Device 2 structure
+
+	pthread_mutex_t memoryMutexes[MUTEXARRSIZE]; //an array of mutexes for the p|c memory accessing.
+	pthread_t cpu_thread; //cpu thread
+	pthread_t IO1_thread; //IO1 thread
+	pthread_t IO2_thread; //IO2 thread
+	pthread_t Keyboard_thread; //keyboard thread
+	//pthread_cond_t timer; //???????
+	//pthread_cond_t keyboarddone; //???????
+	//pthread_cond_t switchcomplete; //???????
+	//pthread_mutex_t mutex; //???????
+
+
+} cpuObj;
+
+typedef cpuObj *cpuPtr;
+
+cpuPtr cpuConstructor();
+
+void CPURun(cpuPtr);
+
+void InterruptHandler(int, PCBPtr);
+
+#endif /* CPU_H_ */
