40a6ae6a43e704b724f8c1d1d9cf23df5bec9421
anelson7@uw.edu <anelson7@uw.edu@c1dd0df8-851b-c1e7-a82f-bfbec5034791>
2013-06-03 06:38:31 +0000

    MyVersion2

---
+++ Keyboard.c
@@ -0,0 +1,46 @@
+/*
+cccc * Keyboard.c
+ *
+ *  Created on: May 30, 2013
+ *      Author: Aaron Nelson
+ */
+
+#ifndef KEYBOARD_C_
+#define KEYBOARD_C_
+
+#include "global.h"
+#include "Keyboard.h"
+
+KBDevPtr KBDevConstructor() {
+	KBDevPtr keyboard = (KBDevPtr) malloc(sizeof(KBDev));
+
+	keyboard->keyboardFree = 0;
+	//keyboard->reset = PTHREAD_COND_INITIALIZER;
+	//keyboard->mutex = PTHREAD_MUTEX_INITIALIZER;
+	keyboard->owner = NULL;
+
+	//pthread_create(&keyboard->keyboard_thread, NULL, KBDevRun, (void *) keyboard);
+
+	return keyboard;
+}
+
+void KBDevDestructor(KBDevPtr this) {
+	//pthread_cancel(this->keyboard_thread);
+	free(this);
+}
+
+void KBDevRun(void *args) {
+	KBDevPtr aKB = (KBDevPtr) args;
+
+	while(1) {
+		if (aKB->owner != NULL) {
+			if (kbhit()) {
+				KBINT = 1;
+				//InterruptHandler(5, aKB->owner);
+				//pthread_cond_wait(&aKB->reset, &aKB->mutex);
+			}
+		}
+	}
+}
+
+#endif /* KEYBOARD_C_ */
